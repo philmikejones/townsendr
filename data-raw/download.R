f53c573af4949b585cbed8df10dcddbc32c9ff0b
Phil Mike Jones <philmikejones@gmail.com>
2018-09-09 10:24:12 +0100

    update download script

--- data-raw/download.R
+++ data-raw/download.R
@@ -3,19 +3,24 @@ library("stringr")
 library("nomisr")
 
 nomis_id = data.frame(
-  id   = c("NM_541_1", "NM_556_1", "NM_621_1", "NM_619_1"),
+  id   = c("NM_541_1", "NM_556_1", "NM_619_1", "NM_621_1"),
   name = c("persons_per_room", "unemployed", "car", "tenure"),
   stringsAsFactors = FALSE
 )
 
-nomis_geography = data.frame(
-  id        = c("TYPE464"),
-  geography = c("district"),
-  stringsAsFactors = FALSE
-)
+# geographies look the same
+nomis_get_metadata("NM_541_1", "geography", "TYPE")
+nomis_get_metadata("NM_556_1", "geography", "TYPE")
+nomis_get_metadata("NM_619_1", "geography", "TYPE")
+nomis_get_metadata("NM_621_1", "geography", "TYPE")
 
-nomis_measure = data.frame(
-  measures    = c(20100),
-  description = c("value"),
-  stringsAsFactors = FALSE
-)
+# measures are 20100 = value; 20301 = percent
+# all are the same
+nomis_get_metadata("NM_541_1", "measures")
+nomis_get_metadata("NM_556_1", "measures")
+nomis_get_metadata("NM_619_1", "measures")
+nomis_get_metadata("NM_621_1", "measures")
+
+nomis_get_metadata("NM_541_1", "C_PPROOMHUK11")
+
+nomis_get_data("NM_556_1", geography = "TYPE480")  # region
