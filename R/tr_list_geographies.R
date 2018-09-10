1e96d10883e020cf13e7c68733e5e71d75dd3969
Phil Mike Jones <philmikejones@gmail.com>
2018-09-10 16:25:04 +0100

    update list_geographies()

--- R/tr_list_geographies.R
+++ R/tr_list_geographies.R
@@ -12,9 +12,23 @@ tr_list_geographies = function(year) {
 
   geographies = lapply(
     nomis_id$id[nomis_id$year == year],
-    nomis_get_metadata, "geography", "TYPE"
+    nomisr::nomis_get_metadata, "geography", "TYPE"
   )
+
+  geographies_length = length(geographies)  # for filter()
+
+  geographies = do.call(rbind, geographies)
+
+  ids = table(geographies$id)
+  # ids[1] = 3
+  ids = ids[ids == geographies_length]
+  ids = attr(ids, "dimnames")
+
+  geographies = geographies[ids %in% geographies$id, ]
 
   geographies
 
+
 }
+
+tr_list_geographies(2011)
