3fcca245d8b74e208b6d0e56c26c17344dcdc92b
Phil Mike Jones <philmikejones@gmail.com>
2018-09-10 16:24:55 +0100

    add tr_calc_townsend() skeleton

---
+++ R/tr_calc_townsend.R
@@ -0,0 +1,14 @@
+#' tr_calc_townsend
+#'
+#' Calculate Townsend Material Deprivation scores for the specified geography.
+#'
+#' @param year a census year (2011, 2001, 1991, 1981)
+#' @param geography the geography you wish to calculate Townsend scores for
+#'
+#' @return
+#' @export
+#'
+#' @examples
+tr_calc_townsend <- function(year = NULL, geography = NULL) {
+
+}
