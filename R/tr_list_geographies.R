6b121c9a29943d7f481a186a1b844645be300add
Phil Mike Jones <philmikejones@gmail.com>
2018-09-10 21:45:52 +0100

    update list_geographies()

--- R/tr_list_geographies.R
+++ R/tr_list_geographies.R
@@ -7,26 +7,47 @@
 #'
 #' @examples
 #' tr_list_geographies(2011)
-tr_list_geographies = function(year) {
+tr_list_geographies <- function(year) {
 
-  geographies = lapply(
+  tr_check_year(year)
+
+  geographies <- lapply(
     townsendr:::nomis_ids$id[townsendr:::nomis_ids$year == year],
     nomisr::nomis_get_metadata, "geography", "TYPE"
   )
 
-  geographies_length = length(geographies)  # for filter()
+  if (length(geographies) == 0L) {
+    message("No results for specified year")
+  }
+  geographies_length <- length(geographies)  # for filter()
 
-  geographies = do.call(rbind, geographies)
+  # Remove any geographies that aren't present in all returned tables
+  df_check <- lapply(geographies, is.data.frame)
+  if (!all(unlist(df_check))) {
+    stop("Results returned malformed. Check year")
+  }
+  geographies <- do.call(rbind, geographies)
 
-  geographies =
+  if (nrow(geographies) < geographies_length) {
+    stop("Results returned malformed. Check year")
+  }
+
+  geographies_n_groups <-
+    geographies %>%
+    dplyr::group_by(id) %>%
+    dplyr::n_groups()
+
+  geographies <-
     geographies %>%
     dplyr::group_by(id) %>%
     dplyr::filter(dplyr::n() == geographies_length) %>%
     dplyr::ungroup() %>%
     dplyr::filter(!duplicated(.))
 
+  if (geographies_n_groups != nrow(geographies)) {
+    stop("Cannot obtain geographies. Check year")
+  }
+
   geographies
 
 }
-
-test = tr_list_geographies(2011)
