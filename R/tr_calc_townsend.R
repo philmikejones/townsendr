f442b787261ab42feab3db2156ca35e9f95d6ca6
Phil Mike Jones <philmikejones@gmail.com>
2018-09-10 16:36:52 +0100

    update tr_calc_townsend() docs

--- R/tr_calc_townsend.R
+++ R/tr_calc_townsend.R
@@ -1,14 +1,17 @@
 #' tr_calc_townsend
 #'
 #' Calculate Townsend Material Deprivation scores for the specified geography.
+#' Geographies are specified using the Nomis API format, and can be found using
+#' tr_list_geographies()
 #'
 #' @param year a census year (2011, 2001, 1991, 1981)
 #' @param geography the geography you wish to calculate Townsend scores for
 #'
-#' @return
+#' @return A tibble with census year, geography code, geography name, and results
 #' @export
 #'
 #' @examples
+#' tr_calc_townsend(2011, "TYPE480")
 tr_calc_townsend <- function(year = NULL, geography = NULL) {
 
 }
