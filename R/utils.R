3177f487354d3d2e9c44dc344a7b6b285b6c4092
Phil Mike Jones <philmikejones@gmail.com>
2018-09-11 11:47:33 +0100

    quiet R CMD checks for . global variable

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,23 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+## See: https://github.com/STAT545-UBC/Discussion/issues/451#issuecomment-264598618
+## I've add 'id' as this is also a global that the user does not have access to
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "id"))
+
+
+## tr_check_year function tests year input for multiple functions
 tr_check_year <- function(year = NULL) {
 
   if (is.null(year)) {
