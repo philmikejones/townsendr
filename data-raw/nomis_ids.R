817788e2f8e61a767a16c54ac8c60c985b026365
Phil Mike Jones <philmikejones@gmail.com>
2018-09-11 12:36:29 +0100

    add 2001 census to nomis_ids

--- data-raw/nomis_ids.R
+++ data-raw/nomis_ids.R
@@ -1,8 +1,23 @@
-nomis_ids = data.frame(
-  id   = c("NM_541_1", "NM_556_1", "NM_619_1", "NM_621_1"),
-  year = c(2011L, 2011L, 2011L, 2011L),
-  name = c("persons_per_room", "unemployed", "tenure", "car"),
-  stringsAsFactors = FALSE
+nomis_ids = tibble::tibble(
+  id   = "NM_621_1",
+  year = 2011L,
+  name = "car"
 )
+
+nomis_ids = rbind(
+  nomis_ids,
+  c("NM_541_1", 2011L, "persons_per_room"),
+  c("NM_619_1", 2011L, "tenure"),
+  c("NM_556_1", 2011L, "unemployed")
+)
+
+nomis_ids =
+  rbind(
+    nomis_ids,
+    c("NM_1679_1", 2001L, "car"),
+    c("NM_1675_1", 2001L, "persons_per_room"),
+    c("NM_1626_1", 2001L, "tenure"),
+    c("NM_1651_1", 2001L, "unemployed")
+  )
 
 usethis::use_data(nomis_ids, internal = TRUE, overwrite = TRUE)
