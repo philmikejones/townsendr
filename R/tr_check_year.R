7b10477100d9a85ff63d67d9e5d026b590e55ad4
Phil Mike Jones <philmikejones@gmail.com>
2018-09-11 10:54:21 +0100

    update check_year and check_year tests

--- R/tr_check_year.R
+++ R/tr_check_year.R
@@ -15,17 +15,17 @@
 tr_check_year <- function(year = NULL) {
 
   if (is.null(year)) {
-    stop("Census year is not specified")
+    stop("year is not specified")
   }
 
-  year <- as.integer(year)
+  year <- suppressWarnings(as.integer(year))  # check for errors next
 
   if (is.na(year)) {
     stop("year is not a number")
   }
 
   if (year != 2011L && year != 2001L && year != 1991 && year != 1981) {
-    stop("Year is not a census year")
+    stop("year is not a census year")
   }
 
 }
--- tests/testthat/test-tr_check_year.R
+++ tests/testthat/test-tr_check_year.R
@@ -1,13 +1,7 @@
 context("tr_check_year()")
 
-test_that("Missing year returns an error", {
-  expect_error(tr_check_year(), "Census year is not specified")
-})
-
-test_that("Non census year returns an error", {
-  expect_error(tr_check_year(year = 2018L), "not a census year")
-})
-
-test_that("non-integer year returns an error", {
+test_that("Incorrect input returns an error", {
+  expect_error(tr_check_year(), "year is not specified")
   expect_error(tr_check_year("twenty-eleven", "year is not a number"))
+  expect_error(tr_check_year(year = 2018L), "not a census year")
 })
