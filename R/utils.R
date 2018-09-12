310c3e32e5e5e2656edba97ab72bbcca990c86b7
Phil Mike Jones <philmikejones@gmail.com>
2018-09-12 11:38:07 +0100

    add global variables

--- R/utils.R
+++ R/utils.R
@@ -18,8 +18,13 @@ NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 ## https://github.com/STAT545-UBC/Discussion/issues/451#issuecomment-264598618
-## I've add 'id' as this is also a global that the user does not have access to
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "id"))
+## added 'id' as this is also a global that the user does not have access to
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(
+    ".", "id", "CELL_NAME", "GEOGRAPHY_CODE", "GEOGRAPHY_NAME",
+    "OBS_VALUE"
+  ))
+}
 
 
 ## tr_check_year function tests year input for multiple functions
@@ -181,47 +186,3 @@ tr_shape_data <- function(tr_data) {
   tr_data
 
 }
-
-# car <-
-#   car %>%
-#   dplyr::filter(
-#     CELL_NAME == "No cars or vans in household"
-#   ) %>%
-#   dplyr::mutate(CELL_NAME = "car") %>%
-#   dplyr::select(GEOGRAPHY_CODE, GEOGRAPHY_NAME, CELL_NAME, OBS_VALUE)
-#
-#
-# persons_per_room <-
-#   persons_per_room %>%
-#   dplyr::filter(
-#     stringr::str_detect(
-#       C_PPROOMHUK11_NAME,
-#       "Over 1.0 and up to 1.5 persons per room|Over 1.5 persons per room"
-#     )
-#   ) %>%
-#   dplyr::group_by(GEOGRAPHY_CODE, GEOGRAPHY_NAME) %>%
-#   dplyr::summarise_if(is.numeric, sum) %>%
-#   dplyr::ungroup() %>%
-#   dplyr::mutate(CELL_NAME = "persons_per_room") %>%
-#   dplyr::select(GEOGRAPHY_CODE, GEOGRAPHY_NAME, CELL_NAME, OBS_VALUE)
-
-#
-# tenure <-
-#   tenure %>%
-#   dplyr::filter(
-#     CELL_NAME == "Social rented" |
-#       CELL_NAME == "Private rented" |
-#       CELL_NAME == "Living rent free"
-#   ) %>%
-#   dplyr::group_by(GEOGRAPHY_CODE, GEOGRAPHY_NAME) %>%
-#   dplyr::summarise_if(is.numeric, sum) %>%
-#   dplyr::ungroup() %>%
-#   dplyr::mutate(CELL_NAME = "tenure") %>%
-#   dplyr::select(GEOGRAPHY_CODE, GEOGRAPHY_NAME, CELL_NAME, OBS_VALUE)
-#
-#
-# unemployed <-
-#   unemployed %>%
-#   dplyr::filter(CELL_NAME == "Economically active: Unemployed") %>%
-#   dplyr::mutate(CELL_NAME = "unemployed") %>%
-#   dplyr::select(GEOGRAPHY_CODE, GEOGRAPHY_NAME, CELL_NAME, OBS_VALUE)
