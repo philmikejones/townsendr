8b702faa58a380a046a8ba3118d6645d1a0cf32a
Phil Mike Jones <philmikejones@gmail.com>
2018-09-12 16:26:12 +0100

    add test for tr_label_data()

--- tests/testthat/test-tr_calc_townsend.R
+++ tests/testthat/test-tr_calc_townsend.R
@@ -46,3 +46,12 @@ test_that("incorrect input errors", {
     tr_label_data(c("dat1", "dat2")), "not a data frame"
   )
 })
+
+dat = tr_label_data(dat)
+test_that("correct input is correctly labelled", {
+  expect_true(all.equal(
+    colnames(dat), c(
+      "geography_code", "geography_name", "CELL_NAME", "OBS_VALUE"
+    )
+  ))
+})
